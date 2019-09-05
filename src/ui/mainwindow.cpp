aba8374c198c1d031e7384f745fdd7d4420cf6c8
Xcrawl <yx131126@gmail.com>
2019-09-05 18:12:56 +0800

    @feat:3dRender and 2dImgRender convert freely

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -35,6 +35,10 @@ void MainWindow::initialize()
     stlActor_ = vtkSmartPointer<vtkActor>::New();
     polyDataMapper_ = vtkSmartPointer<vtkPolyDataMapper>::New();
 
+    jpgReader_ = vtkSmartPointer<vtkJPEGReader>::New();
+    jpgWriter_ = vtkSmartPointer<vtkJPEGWriter>::New();
+    imageActor_ = vtkSmartPointer<vtkImageActor>::New();
+    imgMapper_ = vtkSmartPointer<vtkImageMapper>::New();
 
     qDebug()<<"initialize end"<<endl;
 }
@@ -43,29 +47,31 @@ void MainWindow::initUIInterface()
 {
     qDebug()<<"initializeUIInterface start"<<endl;
     renderer_ = vtkSmartPointer<vtkRenderer>::New();
+    imgRenderer_ = vtkSmartPointer<vtkRenderer>::New();
     renderWindow_ = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
 
     main3DOpenGLWidget_ = new QVTKOpenGLWidget;
-
-    main3DOpenGLWidget_->setFixedSize(QSize(800,ui->model3D->height()));
+    this->ui->model3D->setFixedHeight(600);
+    this->ui->model3D->setFixedWidth(1200);
+    main3DOpenGLWidget_->setFixedSize(QSize(800,600));
     qDebug()<<"************"<<endl;
+
     main3DOpenGLWidget_->SetRenderWindow(renderWindow_);
-
-
-
-
+    main3DOpenGLWidget_->update();
     vLayout_ = new QVBoxLayout;
     vLayout_->addWidget(main3DOpenGLWidget_);
-
     ui->model3D->setLayout(vLayout_);
 
     qDebug()<<"initializeUIInterface end"<<endl;
 }
 
 
-void MainWindow::on_openFile_triggered()
+void MainWindow::on_openSTLFile_triggered()
 {
     qDebug()<<"open file trigger start"<<endl;
+
+
+
 
 
     const auto file_to_open = QFileDialog::getOpenFileName(this,tr("Open STL file"),"",tr("STL Files(*.stl)"));
@@ -89,20 +95,81 @@ void MainWindow::on_openFile_triggered()
     }
 }
 
+void MainWindow::on_open2DImage_triggered()
+{
+    qDebug()<<"open 2d image trigger start"<<endl;
+
+
+    QString file_to_open = QFileDialog::getOpenFileName(this,tr("Open 2d image"),"",tr("JPEG Files(*.jpg);;JPEG Files(*.jpeg)"));
+    qDebug()<<file_to_open<<endl;
+    if(file_to_open.isNull())
+    {
+          return;
+    }
+    imgPath_ = file_to_open;
+    try
+    {
+        twoDImageShow();
+    }
+    catch(const std::exception &error)
+    {
+        qDebug() << error.what() << endl;
+    }
+
+
+
+
+}
+
 void MainWindow::modelDisplay()
 {
     qDebug()<<"stl render start"<<endl;
 #if 1
     stlReader_->SetFileName(STLPath_.toUtf8().constData());
+    stlReader_->Update();
     polyDataMapper_->SetInputConnection(stlReader_->GetOutputPort());
     stlActor_->SetMapper(polyDataMapper_);
     stlActor_->GetProperty()->SetColor(1,1,1);
 
     renderer_->AddActor(stlActor_);
-    renderWindow_->AddRenderer(renderer_);
-    renderWindow_->Render();
+    renderer_->ResetCamera();
 
+    renderWindow_->RemoveRenderer(imgRenderer_);
+    renderWindow_->AddRenderer(renderer_);
+
+    vtkSmartPointer<vtkRenderWindowInteractor>threeDInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    threeDInteractor->SetRenderWindow(renderWindow_);
+    threeDInteractor->Start();
+    renderWindow_->Render();
 #endif
+
+
+}
+
+void MainWindow::twoDImageShow()
+{
+    qDebug()<<"image render start"<<endl;
+
+    jpgReader_->SetFileName(imgPath_.toUtf8().constData());
+    jpgReader_->Update();
+    imageActor_->SetInputData(jpgReader_->GetOutput());
+    double bounds[6];
+    imageActor_->GetBounds(bounds);
+
+
+
+    imgRenderer_->AddActor(imageActor_);
+    renderWindow_->RemoveRenderer(renderer_);
+    renderWindow_->AddRenderer(imgRenderer_);
+    imgRenderer_->SetBackground(1,1,1);
+    imgRenderer_->ResetCamera();
+    vtkSmartPointer<vtkInteractorStyleImage>interactorImage =
+            vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+    renderWindow_->GetInteractor()->SetInteractorStyle(interactorImage);
+
+    renderWindow_->Render();
+    qDebug()<<"image render end"<<endl;
 
 
 }
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -22,17 +22,27 @@
 #include <vtkPiecewiseFunction.h>
 #include <vtkColorTransferFunction.h>
 #include <vtkImageData.h>
+#include <vtkImageActor.h>
+#include <vtkActor2D.h>
+#include <vtkImageMapper.h>
+#include <vtkJPEGReader.h>
+#include <vtkJPEGWriter.h>
 #include <vtkGPUVolumeRayCastMapper.h>
 #include <vtkFixedPointVolumeRayCastMapper.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkMapper.h>
 #include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkRenderWindowInteractor3D.h>
+#include <vtkCamera.h>
 #include <vtkRenderWidget.h>
 #include <vtkSmartPointer.h>
 #include <vtkNew.h>
 #include <vtkProperty.h>
+#include <vtkProperty2D.h>
 #include <vtkMath.h>
 
+#include <vtkImageInterpolator.h>
 namespace Ui {
 class MainWindow;
 }
@@ -43,14 +53,16 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    virtual ~MainWindow();
     void initialize();
     void initUIInterface();
     void modelDisplay();
+    void twoDImageShow();
 
 
 private slots:
-    void on_openFile_triggered();
+    void on_openSTLFile_triggered();
+    void on_open2DImage_triggered();
 
 private:
     Ui::MainWindow *ui;
@@ -59,15 +71,25 @@ private:
 
     vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow_;
 
+
     vtkSmartPointer<vtkRenderer> renderer_;
-    vtkSmartPointer<vtkSTLReader> stlReader_;
+    vtkSmartPointer<vtkRenderer> imgRenderer_;
+    vtkSmartPointer<vtkJPEGReader>jpgReader_;
+    vtkSmartPointer<vtkSTLReader>stlReader_;
+    vtkSmartPointer<vtkJPEGWriter>jpgWriter_;
     vtkSmartPointer<vtkPolyData> polyData_;
 
     vtkSmartPointer<vtkPolyDataMapper> polyDataMapper_;
     vtkSmartPointer<vtkActor> stlActor_;
+    vtkSmartPointer<vtkImageActor> imageActor_;
+    vtkSmartPointer<vtkImageMapper> imgMapper_;
+    vtkSmartPointer<vtkImageInterpolator>imgInterActor_;
+    vtkSmartPointer<vtkCamera>tmpCamera_;
+
 
 
     QString STLPath_;
+    QString imgPath_;
     QString outPutPath_;
 
 
